a5235a7754354170a79012e3fd023b1589020f02
Soojung Kim <soojung.k@navercorp.com>
2019-07-02 18:25:57 +0900

    # ch2. word representation

--- common/util.py
+++ common/util.py
@@ -14,3 +14,126 @@ def clip_grads(grads, max_norm):
     if rate < 1:
         for grad in grads:
             grad *= rate
+
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
+
+    word_to_id = {}
+    id_to_word = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+
+    corpus = np.array([word_to_id[w] for w in words])
+
+    return corpus, word_to_id, id_to_word
+
+
+def cos_similarity(x, y, eps=1e-8):
+    '''코사인 유사도 산출
+
+    :param x: vector
+    :param y: vector
+    :param eps: 0으로 나누기를 방지하기 위한 작은값
+    :return:
+    '''
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
+
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    '''유사 단어 검색
+
+    :param query: 쿼리
+    :param word_to_id: 단어에서 단어 ID로 변환하는 딕셔너리
+    :param id_to_word: 단어ID에서 단어로 변환하는 딕셔너리
+    :param word_matrix: 단어 벡터를 정리한 행렬. 각 행에 해당 단어 벡터가 저장되어 있다고 가정
+    :param top: 상위 몇 개까지 출력할 지 지정
+    :return:
+    '''
+    if query not in word_to_id:
+        print(f'{query}를 찾을 수 없습니다.')
+        return
+
+    print(f'\n[query] {query}')
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    # calculate cosine similarity
+    vocab_size = len(id_to_word)
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+
+    # print cos sim desc
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print(f'{id_to_word[i]} : {similarity[i]}')
+
+        count += 1
+        if count >= top:
+            return
+
+
+
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    '''create co-occurrence matrix
+
+    :param corpus: 말뭉치(단어 ID 목록)
+    :param vocab_size: 어휘 수
+    :param window_size: 윈도우 크기
+    :return: 동시발생 행렬
+    '''
+    corpus_size= len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size+1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
+
+
+def ppmi(C, verbose=False, eps=1e-8):
+    '''positive pointwise mutual information
+
+    :param C: co-occurrence matrix
+    :param verbose: progress
+    :return:
+    '''
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[j]*S[i]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total//100) == 0:
+                    print('%.1f%% done' % (100*cnt/total))
+    return M
+
+
+
